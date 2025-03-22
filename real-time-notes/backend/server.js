bf9e4d4ab345ed1a503c10aa86afb0531e71ec31
wachira567 <wachiramboche@gmail.com>
2025-03-22 20:30:27 +0300

    commit

--- real-time-notes/backend/server.js
+++ real-time-notes/backend/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: ["https://real-time-notes.vercel.app"],
+    origin: ["https://real-time-communication-with-socket-io.vercel.app/"],
     methods: ["GET", "POST"]
   }
 });
